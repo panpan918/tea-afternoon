a90e3b9d2f4846b87c4a8e785ba4bdde84e41b99
panpan918 <88171800@qq.com>
2021-02-01 00:28:45 +0800

    first

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,24 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ]
+  ],
+  productionSourceMap:false,
+  css:{
+    loaderOptions:{
+      postcss:{
+        plugins:[
+          require('postcss-pxtorem')({
+            rootValue:16,
+            unitPrecision:5,
+            propList:['font','font-size','line-height','letter-spacing'],
+            selectorBlackList:[],
+            replace:true,
+            mediaQuery:false,
+            minPixelValue:0,
+            exclude:/node_modules/i
+          }),
+        ]
+      }
+    }
+  },
 }
